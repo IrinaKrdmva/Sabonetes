9dc4929551fa15bfe5d6ff11b675bcb09957f96d
Irina Karadimova <irinamaple84@gmail.com>
2024-09-12 17:41:27 +0100

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,10 +1,10 @@
-<footer class="container-fluid p-4 m-2">
-    <div class="row mt-5">
-        <div class="p col-12 text-center">
-            Copyright &copy 2024 Irina Karadimova. Todos os direitos reservados.
+    <footer class="container-fluid p-4 m-2">
+        <div class="row mt-5">
+            <div class="p col-12 text-center">
+                Copyright &copy 2024 Irina Karadimova. Todos os direitos reservados.
+            </div>
         </div>
-    </div>
-</footer>
-</body>
+    </footer>
+    </body>
 
-</html>
+    </html>
